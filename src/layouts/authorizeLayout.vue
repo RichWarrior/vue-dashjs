5354910ea0fce4f3563a7986250da5a7710fe39a
Ömer Faruk Şahin <omerfaruk@cloudberry.com.tr>
2019-10-31 17:00:08 +0300

    Page Transition

--- src/layouts/authorizeLayout.vue
+++ src/layouts/authorizeLayout.vue
@@ -3,7 +3,9 @@
     <dashjs-toolbar v-on:DrawerStatusChange="DrawerStatusChange" :drawerStatus="drawerStatus"></dashjs-toolbar>
     <dashjs-sidebar :drawerStatus="drawerStatus" v-on:transtionend="transtionend"></dashjs-sidebar>
     <v-content class="mb-5">
-      <router-view></router-view>
+      <transition name="fade" mode="out-in">
+        <router-view />
+      </transition>
     </v-content>
     <dashjs-footer :drawerStatus="drawerStatus"></dashjs-footer>
   </v-app>
@@ -12,7 +14,8 @@
 <script>
 export default {
   data: () => ({
-    drawerStatus: true
+    drawerStatus: true,
+    prevHeight: 0
   }),
   methods: {
     DrawerStatusChange() {
@@ -24,3 +27,17 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
--- src/layouts/unauthorizeLayout.vue
+++ src/layouts/unauthorizeLayout.vue
@@ -1,3 +1,17 @@
 <template>
     <h1 class="title">UnAuthorize</h1>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
