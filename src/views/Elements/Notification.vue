666becfcb4c43bd1fc1cb8fb970c897365840000
omerfaruk@cloudberry.com.tr <omerfaruk@cloudberry.com.tr>
2019-12-14 18:26:59 +0300

    close #49

--- src/views/Elements/Notification.vue
+++ src/views/Elements/Notification.vue
@@ -6,16 +6,21 @@
     <v-col cols="12" md="6" sm="12" class="pa-1">
       <sweetalert />
     </v-col>
+    <v-col cols="12" md="6" sm="12" class="pa-1">
+      <snackbar />
+    </v-col>
   </v-row>
 </template>
 
 <script>
 import toast from "@/components/Notifications/Toast";
 import sweetalert from "@/components/Notifications/SweetAlert2";
+import snackbar from '@/components/Notifications/Snackbar';
 export default {
   components: {
     toast,
-    sweetalert
+    sweetalert,
+    snackbar
   }
 };
 </script>
