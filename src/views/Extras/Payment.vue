2e81e9ed4442784c4c9defa31beddd5b58b05a32
unknown <faruk_thecno@hotmail.com>
2019-12-27 20:48:24 +0300

    close #50

--- src/views/Extras/Payment.vue
+++ src/views/Extras/Payment.vue
@@ -1,3 +1,44 @@
 <template>
-    <h1 class="title">Payment Form</h1>
+  <div class="wrapper" id="app">
+    <CardForm
+      :form-data="formData"
+      @input-card-number="updateCardNumber"
+      @input-card-name="updateCardName"
+      @input-card-month="updateCardMonth"
+      @input-card-year="updateCardYear"
+      @input-card-cvv="updateCardCvv"
+    />
+  </div>
 </template>
+
+<script>
+import CardForm from "@/components/Payment/CardForm";
+export default {
+  name: "app",
+  components: {
+    CardForm
+  },
+  data() {
+    return {
+      formData: {
+        cardName: "",
+        cardNumber: "",
+        cardMonth: "",
+        cardYear: "",
+        cardCvv: ""
+      }
+    };
+  },
+  methods: {
+    updateCardNumber() {},
+    updateCardName() {},
+    updateCardMonth() {},
+    updateCardYear() {},
+    updateCardCvv() {}
+  }
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/payment/style.scss";
+</style>
